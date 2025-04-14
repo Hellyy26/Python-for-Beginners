a1c390d081fb37bbaad4d8696a1702edcf859e6c
Hellyy26 <vyashellu@gmail.com>
2025-04-14 19:08:25 +0530

    Add files via upload

---
+++ hellyimport.py
@@ -0,0 +1,5 @@
+# this program is created for export
+def welcome():
+    print("hellooo welcome")
+
+h1="hello world"    
---
+++ import.py
@@ -0,0 +1,31 @@
+# import in python is the process of loading code from python file into the
+# current script  
+
+# import math
+# result=math.sqrt(9)
+# print(result)
+# res=math.log(3)
+# print(res)
+
+
+
+# from keywordused to impoer specific function only
+# from math import pi,floor
+# res2=floor(3.5643)
+# print(pi)
+# print(res2)
+
+
+# as keyword
+# import math as m
+# print(pi)
+
+
+# dir function gives all function value all the math dunction display
+# import math
+# print(dir(math))
+
+from hellyimport import welcome,h1
+print(h1)
+welcome() 
+
